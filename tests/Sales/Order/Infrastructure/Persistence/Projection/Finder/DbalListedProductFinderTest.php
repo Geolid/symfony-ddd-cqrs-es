5cd41b0395c58ad780d389d209312c9e691f0dc5
qberlemont <q.berlemont@geolid.com>
2026-08-14 08:58:05 +0200

    test(order): collapse DbalListedProductFinderTest into one case per byIds()

--- tests/Sales/Order/Infrastructure/Persistence/Projection/Finder/DbalListedProductFinderTest.php
+++ tests/Sales/Order/Infrastructure/Persistence/Projection/Finder/DbalListedProductFinderTest.php
@@ -26,44 +26,15 @@ final class DbalListedProductFinderTest extends AbstractIntegrationTestCase
     {
         // Given
         $cups = ProductTestFactory::new()->withLabel('Espresso cups, set of 6')->withUnitAmountInCents(1_750)->store();
-        $saucer = ProductTestFactory::new()->withLabel('Saucer')->withUnitAmountInCents(83)->store();
         ProductTestFactory::new()->withLabel('Untouched')->withUnitAmountInCents(500)->store();
-
-        // When
-        $results = iterator_to_array($this->finder->byIds($cups->id()->toString(), $saucer->id()->toString()));
-
-        // Then
-        self::assertCount(2, $results);
-        $byId = [];
-        foreach ($results as $result) {
-            $byId[$result->productId] = $result;
-        }
-        self::assertSame('Espresso cups, set of 6', $byId[$cups->id()->toString()]->label);
-        self::assertSame(1_750, $byId[$cups->id()->toString()]->unitAmountInCents);
-        self::assertSame('Saucer', $byId[$saucer->id()->toString()]->label);
-        self::assertSame(83, $byId[$saucer->id()->toString()]->unitAmountInCents);
-    }
-
-    #[Test]
-    public function itFiltersOutUnknownIds(): void
-    {
-        // Given
-        $cups = ProductTestFactory::new()->withLabel('Espresso cups, set of 6')->withUnitAmountInCents(1_750)->store();
 
         // When
         $results = iterator_to_array($this->finder->byIds($cups->id()->toString(), Uuid::uuid7()->toString()));
 
         // Then
         self::assertCount(1, $results);
-    }
-
-    #[Test]
-    public function itFindsNothingWhenNoIdMatches(): void
-    {
-        // When
-        $results = iterator_to_array($this->finder->byIds(Uuid::uuid7()->toString()));
-
-        // Then
-        self::assertCount(0, $results);
+        self::assertSame($cups->id()->toString(), $results[0]->productId);
+        self::assertSame('Espresso cups, set of 6', $results[0]->label);
+        self::assertSame(1_750, $results[0]->unitAmountInCents);
     }
 }
